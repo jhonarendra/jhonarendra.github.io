fcfa4144570e79089237a021458544a597a67f81
jhonarendra <jhonarendra@gmail.com>
2021-10-30 20:50:14 +0800

    add: popup

--- _nuxt/static/1635568075/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fadmin","\u002Fcontact","\u002Fblog","\u002Fabout","\u002Fproject","\u002Fblog\u002Ftesting","\u002Fblog\u002Fhello-world","\u002Fblog\u002Fmy-github-pages","\u002Fblog\u002Fblog-post","\u002Fblog\u002Fada-audit","\u002Fblog\u002Fexample-full-markdown","\u002Fblog\u002Fmy-second-blog","\u002Fblog\u002Fmy-first-blog-post"]})
